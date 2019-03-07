8fa67b55e9ee772a79f16f592839ecb74c8bc4d3
Tobe O <thosakwe@gmail.com>
2019-03-06 23:37:52 -0500

    base entry point

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,16 @@
+open Arg
+
 let () =
-  print_endline "Todo"
+  let compile_only = ref false in
+  let inputs : string list ref = ref [] in
+  let specs = [
+    ("-c", Set compile_only, "Compile only, do not link.")
+  ] in
+  let anon s =
+    let current = !inputs in
+    let new_value = List.append current [s] in
+    inputs := new_value
+  in
+  parse specs anon "usage: wavo [options...] <input_files>";
+  print_endline ("Count: " ^ (string_of_int (List.length !inputs)))
+  
